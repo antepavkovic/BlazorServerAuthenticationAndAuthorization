754e7a8635a77069abb7300cbe74cdc5bee3ad0e
antepavkovic <ante.pavkovic357@gmail.com>
2022-10-29 15:14:17 +0200

    Add project files.

---
+++ Authentication/UserAccountService.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BlazorServerAuthenticationAndAuthorization.Authentication
+{
+    public class UserAccountService
+    {
+        string pass1 = "admin";
+
+        private List<UserAccount> _users;
+        
+        public UserAccountService()
+        {
+           
+            _users = new List<UserAccount>
+            {
+                new UserAccount{ UserName = "admin",Password =pass1 }
+               
+            };
+        }
+        
+        public UserAccount? GetByUserName(string userName)
+        {
+            return _users.FirstOrDefault(x => x.UserName == userName);
+        }
+    }
+ 
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+﻿Aplikacija je rađena u .NET 6.0 framework-u.
